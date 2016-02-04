bee3eebaa35dff5373d5201cd7c1ec3d6c47e466
Drastorm <colin_drastorm@live.co.uk>
2016-02-04 08:05:05 +0000

    Wall Jumping
    Players now slide down walls and can jump from them.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour 
+{
+	/*
+	public float movementSpeed;
+	public float moveHorizontal;
+	public float defaultSpeed;
+	public float jumpVelocity;
+
+	public bool facingRight = true;
+
+	public bool grounded = false;
+	public bool wallContact = false;
+	public Transform groundCheckA;
+	public Transform groundCheckB;
+	public Transform leftWallCheckA;
+	//public Transform leftWallCheckB;
+	//public Transform rightWallCheckA;
+	public Transform rightWallCheckB;
+	public LayerMask whatIsGround;
+	public LayerMask whatIsWall;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+*/
+	Controller2D controller;
+
+	public float movementSpeed = 5f;
+	public float JumpHeight = 5f;
+	public float timeToJumpApex = 0.5f;
+	public float wallSlideSpeedMax = 1f;
+	public Vector2 wallJumpClimb;
+	public Vector2 wallJumpOff;
+	public Vector2 wallLeap;
+	public bool facingRight = true;
+	float gravity;
+	float jumpVelocity;
+	Vector3 velocity;
+
+	float velocityXSmoothing;
+	float accelerationTimeAirborne = 0.175f;
+	float accelerationTimeGrounded = 0;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	void Start ()
+	{
+		controller = GetComponent<Controller2D>();
+
+		gravity = -(2 * JumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+		//print ("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
+	}
+
+	void Update ()
+	{
+		Vector2 movement = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		int wallDirX = (controller.collisions.left)? -1: 1;
+
+		float targetVelocityX = movement.x * movementSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+
+		bool wallSliding = false;
+
+		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
+		{
+			wallSliding = true;
+			if (velocity.y < -wallSlideSpeedMax)
+			{
+				velocity.y = -wallSlideSpeedMax;
+			}
+		}
+
+		if (controller.collisions.above || controller.collisions.below)
+		{
+			velocity.y = 0;
+		}
+
+		if (Input.GetMouseButtonDown(0) && Time.time > nextFire && wallSliding == false)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			if (wallSliding == true)
+			{
+				if (wallDirX == movement.x)
+				{
+					velocity.x = -wallDirX * wallJumpClimb.x;
+					velocity.y = wallJumpClimb.y;
+				}
+				else if (movement.x == 0)
+				{
+					velocity.x = -wallDirX * wallJumpOff.x;
+					velocity.y = wallJumpOff.y;
+				}
+				else 
+				{
+					velocity.x = -wallDirX * wallLeap.x;
+					velocity.y = wallLeap.y;
+				}
+			}
+
+			if (controller.collisions.below)
+			{
+				velocity.y = jumpVelocity;
+			}
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+
+		//float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		if (movement.x > 0 && !facingRight)
+		{
+			Flip ();
+			shotSpawn.rotation = Quaternion.Euler (0, 0, 0);
+		}
+		else if (movement.x < 0 && facingRight)
+		{
+			Flip ();
+			shotSpawn.rotation = Quaternion.Euler (0, 180, 0);
+		}
+	}
+
+	/*
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && grounded == true)
+		{
+			grounded = false;
+			GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, jumpVelocity));
+		}
+
+		//GetAxisRaw is used as it returns either 1 or 0 which results in snappier movement
+		moveHorizontal = Input.GetAxisRaw ("Horizontal");
+	}
+
+	void FixedUpdate ()
+	{
+		//grounded returns true when objects with layer selected for whatIsGround is detected in the area between groundCheckA and B
+		//and when player's y velocity is 0
+		grounded = Physics2D.OverlapArea (groundCheckA.position, groundCheckB.position, whatIsGround) && GetComponent<Rigidbody2D>().velocity.y == 0;
+		wallContact = Physics2D.OverlapArea (leftWallCheckA.position, rightWallCheckB.position, whatIsWall);
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveHorizontal * movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		if (moveHorizontal > 0 &&!facingRight)
+		{
+			Flip ();
+			shotSpawn.rotation = Quaternion.Euler (0, 0, 0);
+		}
+		else if (moveHorizontal < 0 &&facingRight)
+		{
+			Flip ();
+			shotSpawn.rotation = Quaternion.Euler (0, 180, 0);
+		}
+	}
+*/
+	void Flip ()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour 
+{
+	public float speed;
+	
+	void Start () 
+	{
+		gameObject.name = "Shot";
+		GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+	}
+	
+	void OnTriggerEnter2D (Collider2D otherObject)
+	{
+		/*
+		if (otherObject.gameObject.tag == "Enemy")
+		{
+			StartCoroutine (OnHit ());
+		}
+		*/
+		if (otherObject.gameObject.tag == "Block")
+		{
+			//StartCoroutine (OnHit ());
+			Destroy(gameObject);
+		}
+	}
+	
+	IEnumerator OnHit ()
+	{
+		yield return new WaitForSeconds (0.1f);
+		Destroy(gameObject);
+	}
+}
